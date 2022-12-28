a077005df687de6deaaa702d340479f9a23d3d29
김영욱 <duddnr787@naver.com>
2022-12-28 20:46:48 +0900

    2022.12.28 2845번

---
+++ beakwook/src/beakwook/Wook2845.java
@@ -0,0 +1,26 @@
+package beakwook;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Wook2845 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	       StringTokenizer st = new StringTokenizer(br.readLine());
+	       StringBuilder sb = new StringBuilder();
+	       
+	       int a = Integer.parseInt((st.nextToken()));
+	       int b = Integer.parseInt((st.nextToken()));
+	       st = new StringTokenizer(br.readLine());
+	       
+	       for(int i = 0; i<5; i++) {
+	    	   int x = Integer.parseInt((st.nextToken()));
+	    	   sb.append(x-a*b + " ");
+	       }
+	       System.out.println(sb);
+	}
+
+}
